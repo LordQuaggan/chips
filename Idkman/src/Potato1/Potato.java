89a9a04868e736ee04b684b3fbcfb7662313e74f
lord quaggan <lord quaggan@kenny>
2017-01-11 00:36:37 -0500

    first change

--- Idkman/src/Potato1/Potato.java
+++ Idkman/src/Potato1/Potato.java
@@ -5,6 +5,8 @@ public class Potato {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Chips");
+		
+		System.out.println("and Dale.");
 	}
 
 }
